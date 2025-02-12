348175fa5a5b16c1e32fed7b9b50b11bb512712e
Dima Korolev <dmitry.korolev+github-2022@gmail.com>
2025-02-11 20:27:31 -0800

    Do not try to run `docker`-based tests on `macos-latest`.

--- run_selftest.sh
+++ run_selftest.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+TEST_SCRIPT_NAME=${1:-pls_go.sh}
+
 pls version || (echo 'Need the `pls` tool installed.' && exit 1)
 echo
 
@@ -20,11 +22,11 @@ SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
   N_PASSED=0
   N_FAILED=0
   cd "$SCRIPT_DIR/.selftest"
-  for T in $(find . -name pls_go.sh | sort); do
+  for T in $(find . -name ${TEST_SCRIPT_NAME} | sort); do
     N_RUN=$((N_RUN+1))
     TN="$(dirname "$T")"
     echo 'Running the test from  `'$TN'/`.'
-    (cd "$TN"; ./pls_go.sh) && N_PASSED=$((N_PASSED+1)) || N_FAILED=$((N_FAILED+1))
+    (cd "$TN"; ./${TEST_SCRIPT_NAME}) && N_PASSED=$((N_PASSED+1)) || N_FAILED=$((N_FAILED+1))
     echo
   done
   echo "Total tests run: $N_RUN, passed: $N_PASSED, failed: $N_FAILED."
