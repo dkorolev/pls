fd50d15091e459a41b711c67d836511158d1ead5
Dima <dmitry.korolev+github-2022@gmail.com>
2025-02-14 18:51:11 -0800

    Testing an Alpine container. (#25)
    * Testing an Alpine container.
    
    * Do not try to run `docker`-based tests on `macos-latest`.
    
    * s/-it/-t/
    
    * `cp -rv 12_docker_alpine 13_docker_alpine_current`.
    
    * Making it work.
    
    * Now using `current` from Alpine.
    
    * Added the forgotten `git` into `apk add`.
    
    * Need the updated golden, not just the updated source.
    
    * Version bump.
    
    * Using `g++` OR `clang++` for instrumentation.

--- pls/static/dot_pls/cc_instrument.sh
+++ pls/static/dot_pls/cc_instrument.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-g++ \
+
+CPP=$(g++ --version >/dev/null && echo g++ || echo clang++)
+
+# NOTE(dkorolev): Unfortunately, this next line _may_fail, which is as expected.
+# As long as `#include "pls.h"` comes before the `#include`-s of modules which may not yet be present.
+
+$CPP \
   -I"$2" \
   -D PLS_INSTRUMENTATION \
   -E \
